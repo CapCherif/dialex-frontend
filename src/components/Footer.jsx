eef051c63c3a68927c71e735a96a1f306517c139
CapCherif <gharbi_cherif@hotmail.com>
2025-04-11 19:20:20 +0200

    correction footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,11 @@ function Footer() {
     <footer>
       <p>© 2025 Parene : Savoir et Partage</p>
       <i>Propulsé par les technologies OpenAI</i>
-      <div>
-        <p><a href="">Politique de confidentialité</a></p>
-        <p><a href="">Conditions d'utilisation</a></p>
-        <p><a href="">Contactez-nous</a></p>
-      </div>
+   
+      <a href="">Politique de confidentialité</a>
+      <a href="">Conditions d'utilisation</a>
+      <a href="">Contactez-nous</a>
+     
       <a href="#" onClick={()=>setHelp(true)}>Aide</a>
     </footer>
   )
