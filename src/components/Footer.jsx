4271527c26b65e7f44a3a0ccf6807a7e1af927c0
abdelghani <abdelghani.demmouche.prof@gmail.com>
2025-05-22 17:40:25 +0200

    add cchgnes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ function Footer() {
       <p>© 2025 Parene : Savoir et Partage</p>
       <i>Propulsé par les technologies OpenAI</i>
    
-      <a href="https://www.parene.org/wp-content/uploads/2025/04/CGU-CGV.pdf">Politique de confidentialité</a>
-      <a href="https://www.parene.org/wp-content/uploads/2025/04/CGU-CGV.pdf" target='_blank'>Conditions d'utilisation</a>
+      <a  href="/politic.pdf">Politique de confidentialité</a>
+      <a href="/cgv.pdf" target='_blank'>Conditions d'utilisation</a>
       <a href="mailto:contact@parene.org">Contactez-nous</a>
      
       <a href="#" onClick={()=>setHelp(true)}>Aide</a>
