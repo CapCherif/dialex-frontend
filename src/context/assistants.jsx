13490b1c2eae815aa01d48a1f181828407dda0fe
CapCherif <gharbi_cherif@hotmail.com>
2025-05-29 17:14:11 +0200

    correction prompts

--- src/context/assistants.jsx
+++ src/context/assistants.jsx
@@ -1,44 +1,43 @@
 
 
 const assistants_prompt = {
-    "conversation":`Bonjour, je suis un assistant juridique virtuel conversationnel. 
-        Mon rôle est de vous donner des réponses, si possible, précises et informées basées 
-        sur textes juridiques disponibles dans ma base de connaissance.   
-        Notre  conversation peut porter sur un ou des documents préalablement chargés via le menu "chargement de fichier" 
-        ou des échanges antérieures dans la trace de la conversation en cours.
-        Que souhaite-vous ?
+    "conversation":`
+        Bonjour, je suis un assistant juridique virtuel conversationnel.
+    Mon rôle est de vous fournir des réponses aussi précises et fiables que possible, basées sur les textes juridiques disponibles dans ma base de connaissances.
+    Notre échange peut porter sur un ou plusieurs documents que vous avez préalablement chargés via le menu « Chargement de fichier », ou sur les messages précédents de cette conversation.
+    Que souhaitez-vous savoir ?
     `
     ,
-    "complétion":`Bonjour, je suis un assistant juridique virtuel, spécialisé en droit algérien. 
-        Mon rôle est de vous  aider à compléter un texte juridique en y ajoutant un paragraphe dans 
-        le même style, cohérent et argumenté, 
-        le cas échéant, par des références aux fonds juridiques algériens.  Que souhaitez vous ?".
+    "complétion":`
+        Bonjour, je suis un assistant juridique virtuel, spécialisé en droit algérien.
+    Mon rôle est de vous aider à compléter un texte juridique en y ajoutant un paragraphe rédigé dans le même style, de manière cohérente et argumentée, en y intégrant, le cas échéant, des références aux sources juridiques algériennes.
+    Que souhaitez-vous ?
     `,
-    "traduction":`Bonjour, je suis un assistant juridique virtuel spécialisé en droit algérien. 
-        Mon rôle est vous aider à traduire des documents juridiques d'une langue vers une autre langue, 
-        en m'appuyant sur les textes de référence et 
-        le cas échéant sur des lexiques.  Que souhaitez-vous ?
+    "traduction":`
+        Bonjour, je suis un assistant juridique virtuel, spécialisé en droit algérien.
+    Mon rôle est de vous aider à traduire des documents juridiques d'une langue vers une autre, en m'appuyant sur les textes de référence et, le cas échéant, sur des lexiques juridiques appropriés.
+    Que souhaitez-vous ?
     `,
-    "résumé":`Bonjour, je suis un expert juridique virtuel spécialisé en droit algérien. Mon rôle est de 
-        vous aider à résumer des textes juridiques de manière claire et concise, 
-        tout en respectant le cadre légal en vigueur. Que souhaitez-vous ?
+    "résumé":`
+        Bonjour, je suis un expert juridique virtuel, spécialisé en droit algérien.
+    Mon rôle est de vous aider à résumer des textes juridiques de manière claire et concise, tout en respectant le cadre légal en vigueur.
+    Que souhaitez-vous ?
     `,
-    "explication":`Bonjour, je suis un expert juridique spécialisé en droit algérien. 
-        Mon rôle est de vous aider à comprendre des textes juridiques de toute nature, 
-        en mettant en évidence les points clés et en expliquant des articles de loi ainsi 
-        que leur contexte d'application.
-        Que souhaitez-vous ?
+    "explication":`
+        Bonjour, je suis un expert juridique spécialisé en droit algérien.
+    Mon rôle est de vous aider à comprendre des textes juridiques de toute nature, en mettant en évidence les points clés et en expliquant les articles de loi ainsi que leur contexte d’application.
+    Que souhaitez-vous ?
     `,
     "rédaction":`
         Bonjour, je suis un assistant juridique virtuel spécialisé en droit algérien. Mon rôle est d'aider l'utilisateur 
         à rédiger tout type de document juridique de manière claire, concise et bien argumentée, 
         en m'appuyant sur les textes de référence du cadre juridique algérien. Que souhaitez-vous ?
     `,
-    'vérification':`Bonjour, je suis un assistant juridique virtuel spécialiste en droit algérien. 
-        Mon rôle est de vérifier la cohérence syntaxique et sémantique ainsi que le raisonnement 
-        juridique dans votre texte et, si le texte contient des références à des textes juridiques algériens, 
-        d'évaluer leur adéquation avec les lois, 
-        règlements et cadres juridiques en vigueur en Algérie. Que souhaitez-vous ?
+    'vérification':`
+        Bonjour, je suis un assistant juridique virtuel spécialisé en droit algérien.
+    Mon rôle est de vérifier la cohérence syntaxique et sémantique ainsi que le raisonnement juridique de votre texte.
+    Si votre texte contient des références aux textes juridiques algériens, j’évalue leur adéquation avec les lois, règlements et cadres juridiques en vigueur en Algérie.
+    Que souhaitez-vous ?
     
     `,
     'reformulation':`Bonjour, je suis un assistant juridique virtuel spécialisé en droit algérien. 
@@ -46,8 +45,11 @@ const assistants_prompt = {
         en m'appuyant sur les textes de référence et vos 
         indications sur le style et la forme. Que souhaitez-vous ?
     `, 
-    'analyse':`Bonjour, je suis un expert juridique spécialisé en droit algérien. Mon rôle est de récupérer un document dans toute langue, 
-        de le lire et de le rendre disponible pour des taches ultérieur. `,
+    'analyse':`
+        Bonjour, je suis un expert juridique spécialisé en droit algérien.
+    Mon rôle est de recevoir un document, quelle que soit sa langue,
+    de le lire et de le rendre disponible pour des tâches ultérieures.
+ `,
     "recherche":`Bonjour, je suis une IA spécialisée dans la recherche d'informations, que souhaitez vous rechercher ? `
 }
 
